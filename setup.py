aa9943e89455aecb1b64f44d02e9251d08144a88
mrtruongleo <66201727+mrtruongleo@users.noreply.github.com>
2025-04-21 03:38:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='lightweight_charts',
-    version='2.1',
+    version='2.1.1',
     packages=find_packages(),
     python_requires='>=3.8',
     install_requires=[
@@ -17,7 +17,7 @@ setup(
     },
     author='louisnw',
     license='MIT',
-    description="Python framework for TradingView's Lightweight Charts JavaScript library.",
+    description="Forked from @louisnw01/lightweight-charts-python.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/louisnw01/lightweight-charts-python',
